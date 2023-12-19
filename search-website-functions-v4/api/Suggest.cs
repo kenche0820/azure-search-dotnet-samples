3621a2b1efc28746f7af7e407d9356d9938556ac
Kenneth Cheung <kenneth.cheung@distributel.ca>
2023-12-19 15:34:27 -0500

    suggest

--- search-website-functions-v4/api/Suggest.cs
+++ search-website-functions-v4/api/Suggest.cs
@@ -15,7 +15,7 @@ namespace WebSearch.Function
     {
         private static string searchApiKey = Environment.GetEnvironmentVariable("SearchApiKey", EnvironmentVariableTarget.Process);
         private static string searchServiceName = Environment.GetEnvironmentVariable("SearchServiceName", EnvironmentVariableTarget.Process);
-        private static string searchIndexName = Environment.GetEnvironmentVariable("SearchIndexName", EnvironmentVariableTarget.Process) ?? "good-books";
+        private static string searchIndexName = Environment.GetEnvironmentVariable("SearchIndexName", EnvironmentVariableTarget.Process) ?? "sharepoint-index";
 
         private readonly ILogger<Lookup> _logger;
 
