65adba4f60adf8924e9a89dc67ec4253f61473ad
Kenneth Cheung <kenneth.cheung@distributel.ca>
2024-01-02 09:37:28 -0500

    Results.js underscore

--- search-website-functions-v4/client/src/components/Results/Results.js
+++ search-website-functions-v4/client/src/components/Results/Results.js
@@ -21,11 +21,12 @@ export default function Results(props) {
 //            console.log("Kenneth checks filenameOutput");              
 //            console.log(filenameOutput);   
             var fileLink = "https://setelab.sharepoint.com/Shared%20Documents/Forms/AllItems.aspx?id=%2FShared%20Documents%2Fdocument%2F"
-            fileLink += filenameOutput
+            fileLink += filenameOutput.replace(/_/g, "%5F");
             fileLink += "&parent=%2FShared%20Documents%2Fdocument&p=true&ga=1"
             console.log("Kenneth checks fileLink");              
             console.log(fileLink);   
             
+
   
   let results = props.documents.map((result, index) => {
     return <Result 
@@ -40,10 +41,9 @@ export default function Results(props) {
 //  let endDocNumber = Math.min(props.skip + props.top, props.count);
 
   return (
-    <div>
-      
-      
-        <p>{answerOutput}</p>
+    <div>   
+        
+        <br><p>{answerOutput}</p></br>
         <p><a href={fileLink}>{filenameOutput}</a></p>
       
     </div>
