9190cd66a01ce097d2030e9e0b63a4d19d323864
Kenneth Cheung <kenneth.cheung@distributel.ca>
2024-01-01 19:37:51 -0500

    Results.js props

--- search-website-functions-v4/client/src/components/Results/Result/Result.js
+++ search-website-functions-v4/client/src/components/Results/Result/Result.js
@@ -4,10 +4,10 @@ import './Result.css';
 
 export default function Result(props) {
     
-    console.log(`result prop = ${JSON.stringify(props)}`)
+ //   console.log(`result prop = ${JSON.stringify(props)}`)
  //   console.log(props.document.content)
 
-//     console.log(`result prop = ${JSON.stringify(props.semanticSearch)}`)
+
     
     return(
     <div className="card result">
--- search-website-functions-v4/client/src/pages/Search/Search.js
+++ search-website-functions-v4/client/src/pages/Search/Search.js
@@ -54,7 +54,8 @@ export default function Search() {
             console.log(finalOutput);    
      
             
-            setResults(response.data.results);
+            setResults(finalOutput);            
+//            setResults(response.data.results);
  //           setFacets(response.data.facets);
 //            setResultCount(response.data.count);
             setIsLoading(false);
